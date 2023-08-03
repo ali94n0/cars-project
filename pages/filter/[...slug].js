4460c4a973e25c1715d0cd15bdf1bc5c2b8e0d4c
ali94n0 <ali94n0@gmail.com>
2023-08-03 19:15:02 +0330

    improve UI/UX and Styles (Finally)

--- pages/filter/[...slug].js
+++ pages/filter/[...slug].js
@@ -11,6 +11,29 @@ function filterPricePage(props) {
   const filteredCars = carsData.filter(
     (car) => car.price >= slugs[0] && car.price <= slugs[1]
   );
+  if (!filteredCars.length)
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <BackBtn route={router} />
+        <div
+          style={{
+            padding: "10px 15px",
+            borderRadius: "5px",
+            backgroundColor: "rgb(252, 90, 100)",
+          }}
+        >
+          <p style={{ color: "#fff " }}>
+            No Cars were Found in the Selected Price Range!
+          </p>
+        </div>
+      </div>
+    );
 
   return (
     <div>
